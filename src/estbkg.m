b620f96c038c61c2ac06ce29ab2cb051cf1612ec
azzuro.huo <azzuro.huo@fb1f9009-b0ef-a1e6-56cd-b83d2361fd0f>
2011-04-11 16:02:49 +0000

    minor change

--- src/estbkg.m
+++ src/estbkg.m
@@ -1,4 +1,4 @@
-function [mu,sigma,F]=estbkg(X,NUMIT)
+function [mu,sigma,F]=estbkg(X,level,NUMIT)
 mu=zeros(NUMIT,1);
 sigma=zeros(NUMIT,1);
 X=X(:);
@@ -6,6 +6,6 @@ F=ones(size(X));
 for k=1:NUMIT
     mu(k)=sum(F.*X)/sum(F);
     sigma(k)=sqrt(sum(((X-mu(k)).*F).^2)/sum(F));
-    F=double(X<=(mu(k)+3*sigma(k)));
+    F=double(X<=(mu(k)+level*sigma(k)));
 end
 return
