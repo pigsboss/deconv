9c4fe0a0f4b0f9887c968bb506044e99e11d90fd
azzuro.huo <azzuro.huo@fb1f9009-b0ef-a1e6-56cd-b83d2361fd0f>
2011-04-14 08:10:21 +0000

    analysis routine of MAP deconvolution

---
+++ trunk/src/deconvmapana.m
@@ -0,0 +1,30 @@
+function [Js,Rs]=deconvmapana(I,REF,PSF,BG,NUMIT)
+%DECONVMAPANA MAP Deconvolution Analysis Routine.
+
+J=I;
+R=REF;
+Js=zeros([numel(I),2*NUMIT]);
+Rs=zeros([numel(I),2*NUMIT]);
+IPSF=rot90(PSF,2);
+H=fftn(ifftshift(PSF));
+IH=fftn(ifftshift(IPSF));
+
+for k=1:NUMIT
+    J=J.*exp(real(ifftn(fftn(I./real(ifftn(H.*fftn(J)))-1).*IH)));
+    R=R.*exp(real(ifftn(fftn(REF./real(ifftn(H.*fftn(R)))-1).*IH)));
+    Js(:,2*(k-1)+1)=J(:);
+    Rs(:,2*(k-1)+1)=R(:);
+%   subplot(4,6,(k-1)*2+1),plot(J-R)
+    subplot(5,8,k),plot(J-R)
+    drawnow
+    J=J.*double(J>=BG)+BG.*double(J<BG);
+    R=R.*double(R>=BG)+BG.*double(R<BG);
+    Js(:,2*(k-1)+2)=J(:);
+    Rs(:,2*(k-1)+2)=R(:);
+%   subplot(4,6,(k-1)*2+2),plot(J-R)
+    drawnow
+%   J=J*(sum(I(:))/sum(J(:)));
+end
+%Js=reshape(Js,[size(I),NUMIT]);
+%Jregs=reshape(Js,[size(I),NUMIT]);
+return
