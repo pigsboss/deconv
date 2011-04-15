8c76b93e527abf10a939046a0df28ec226a0b2ac
azzuro.huo <azzuro.huo@fb1f9009-b0ef-a1e6-56cd-b83d2361fd0f>
2011-04-15 14:11:18 +0000

    minor changes

--- trunk/src/deconvmap.m
+++ trunk/src/deconvmap.m
@@ -1,13 +1,15 @@
-function J=deconvmap(I,PSF,NUMIT,BG)
+function J=deconvmap(I,PSF,BG,NUMIT)
 %DECONVMAP Deconvolution by maximum a posteriori estimate.
 J=I;
 IPSF=rot90(PSF,2);
 H=fftn(ifftshift(PSF));
 IH=fftn(ifftshift(IPSF));
-tic
+%sigma=zeros(NUMIT,1);
+%tic
 for k=1:NUMIT
     J=J.*exp(real(ifft2(fft2(I./real(ifft2(H.*fft2(J)))-1).*IH)));
     J=J.*double(J>=BG)+BG.*double(J<BG);
+%    sigma(k)=std(I(:)-reshape(imconv(J,PSF),numel(I),[]));
 end
-toc
+%toc
 return
