8797d5a5b3a8205c1720b838e26462d2ab9c4058
azzuro.huo <azzuro.huo@fb1f9009-b0ef-a1e6-56cd-b83d2361fd0f>
2011-04-18 04:34:36 +0000

    minor changes

--- src/deconvmca.m
+++ src/deconvmca.m
@@ -9,7 +9,9 @@ o_mpll=zeros(npixels,1); % lower limit of most-probable interval.
 o_mpul=zeros(npixels,1); % upper limit of most-probable interval.
 %Prob_mp=zeros(npixels,1); % probability of most-probable interval.
 prob_pos=zeros(npixels,1); % probability of positive interval.
+%prob_ints=zeros(npixels,nbins+1); % probability of intervals.
 parfor k=1:npixels
+%    [o_mpll(k),o_mpul(k),prob_ints(k,:)]=mostprob(O_src(k,:),nbins);
     [o_mpll(k),o_mpul(k),~]=mostprob(O_src(k,:),nbins);
     prob_pos(k)=sum(O_src(k,:)>0)/npts;
 end
@@ -18,6 +20,7 @@ if length(dim)>2
     o_mpll=reshape(o_mpll,dim(1),[]);
     o_mpul=reshape(o_mpul,dim(1),[]);
     prob_pos=reshape(prob_pos,dim(1),[]);
+%    prob_ints=reshape(prob_ints,dim(1),dim(2),[]);
 end
 result=struct('o_mpll',o_mpll,'o_mpul',o_mpul,'prob_pos',prob_pos);
 return
