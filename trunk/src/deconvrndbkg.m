c82c20875c1b20a20d8376d2b25996a3198aa168
azzuro.huo <azzuro.huo@fb1f9009-b0ef-a1e6-56cd-b83d2361fd0f>
2011-04-19 02:22:47 +0000

    minor change

--- trunk/src/deconvrndbkg.m
+++ trunk/src/deconvrndbkg.m
@@ -1,24 +1,23 @@
-function [O_est,O_src,R_est]=deconvrndbkg(I,P,R,NUMIT)
+function [O_est,O_src]=deconvrndbkg(IMG,PSF,RES,NUMIT)
 % DECONVRNDBKG deconvolution routine regularized by random background
 % constraints (semi-monte-carlo approach).
 % O_opt - Optimal estimate.
 % O_est - Estimated Object.
 % R_est - Estimation (Deconvolution) deviation.
-% I - Observed Image.
-% P - A priori PSF.
-% R - A priori Residual map used to extract background knowledge.
+% IMG - Observed Image.
+% PSF - A priori PSF.
+% RES - A priori Residual map used to extract background knowledge.
 % NUMIT - Number of iterations for internal Bayesian deconvolution method.
-% NSIG - N*sigma significance.
-NUMBG=numel(R);
-R=R(:);
-O_est=zeros(numel(I),NUMBG);
+NUMBG=numel(RES);
+RES=RES(:);
+O_est=zeros(numel(IMG),NUMBG);
 O_src=O_est;
+NPIXELS=numel(IMG);
 tic
 parfor n=1:NUMBG
-    O_est(:,n)=reshape(deconvmap(I,P,R(n),NUMIT),numel(I),[]);
-    O_src(:,n)=O_est(:,n)-R(n);
+    O_est(:,n)=reshape(deconvmap(IMG,PSF,RES(n),NUMIT),NPIXELS,[]);
+    O_src(:,n)=O_est(:,n)-RES(n);
 end
 toc
-R_est=std(size(I),1,2);
-% O_opt=double((O_est-mean(R))>=NSIG*R_est).*O_est+double((O_est-mean(R))<NSIG*R_est)*mean(R);
+save(['mc_',num2str(numel(RES)),'x',num2str(NUMIT)])
 return
